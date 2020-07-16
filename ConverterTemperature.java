0496762e93e826991ee34b733e2fd5838dfdb8bb
olegkoropec <61849825+olegkoropec@users.noreply.github.com>
2020-07-16 17:57:15 +0300

    Add files via upload

--- ConverterTemperature.java
+++ ConverterTemperature.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 public class ConverterTemperature {
     private double x = 0;
-    private double convertTem (double temperature, char convTo){
+    private void convertTem (double temperature, char convTo){
         x = temperature ;
-        if (convTo ==  'F' || convTo =='f')
-            return  x*9/5+32;
-        else if (convTo ==  'C' || convTo == 'c')
-            return   (x-32)*5/9;
+        double y;
+        if (convTo ==  'F' || convTo =='f'){
+            y = x*9/5+32;
+            System.out.printf( "%.2f F", y);}
+        else if (convTo ==  'C' || convTo == 'c'){
+            y = (x-32)*5/9;
+            System.out.printf( "%.2f C", y)   ;}
         else
             System.out.println( "Enter other symbol of temperature. F or C");
-        return 0;
     }
     public static void main(String[] args) {
-    System.out.println( "Enter dimention of temperature.");
+    System.out.println( "Enter temperature.");
     Scanner sc = new Scanner(System.in);
         double  y = sc.nextDouble();
     System.out.println( "What temperature do you want convert to. F or C");
@@ -20,5 +22,4 @@ public class ConverterTemperature {
     ConverterTemperature ct = new ConverterTemperature();
     ct.convertTem(y, ch);
     }
-
 }
